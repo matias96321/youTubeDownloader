1ba1f8034a8a2b22dafcacc383e5823606005909
Matheus Souza Matias <65677371+matias96321@users.noreply.github.com>
2026-06-04 21:12:53 -0300

    Add ignore_no_formats_error option for high quality

--- main.py
+++ main.py
@@ -104,6 +104,7 @@ async def download_video(request: Request, url: str = Query(...), quality: str =
         ydl_opts_download = {
             'format': 'bestvideo+bestaudio',
             'outtmpl': outtmpl,
+            'ignore_no_formats_error': True,
             'quiet': True,
             'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
             'http_headers': {
